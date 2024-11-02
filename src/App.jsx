d9e58149db534bfcdd7cef858c20b77139a703ff
Richard Graebin <77891472+RichardGraebin@users.noreply.github.com>
2024-11-02 17:23:20 -0300

    new project and sidebar skeleton, and input component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import { useState } from "react";
+import SideBar from "./components/SideBar";
+import NewProject from "./components/NewProject";
+
 function App() {
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-screen my-8 flex gap-8">
+      <SideBar />
+      <NewProject />
+    </main>
   );
 }
 
