fc25649d744a788f51f36f295344050acfe395a9
Richard Graebin <77891472+RichardGraebin@users.noreply.github.com>
2024-11-02 17:42:05 -0300

    no project component and button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import SideBar from "./components/SideBar";
 import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 
 function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <SideBar />
-      <NewProject />
+      {/* <NewProject /> */}
+      <NoProjectSelected />
     </main>
   );
 }
