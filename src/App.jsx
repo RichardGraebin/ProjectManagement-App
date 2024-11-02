6bf9c5f7b73f6380a5e993b95e4df3943e274958
Richard Graebin <77891472+RichardGraebin@users.noreply.github.com>
2024-11-02 18:13:21 -0300

    updated button component to accept handleclick function and implemented logic to conditionaly show project component

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,32 @@ import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 
 function App() {
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  let content;
+
+  if (projectState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectState.selectedProjectId === undefined) {
+    content = <NoProjectSelected handleCreateClick={updateProjectId} />;
+  }
+
+  function updateProjectId() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <SideBar />
-      {/* <NewProject /> */}
-      <NoProjectSelected />
+      <SideBar handleCreateClick={updateProjectId} />
+      {content}
     </main>
   );
 }
